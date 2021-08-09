72360be893b0cdada3c0a492edc64430c6352c5a
TOYR <oncwnuEIeXwIdNj8BCJEeZ9IQP3Q@git.weixin.qq.com>
2021-08-09 20:23:33 +0800

    rocketmq 消息监听器

--- src/main/java/org/study/mq/rocketmq/order/OrderProducerTest.java
+++ src/main/java/org/study/mq/rocketmq/order/OrderProducerTest.java
@@ -34,12 +34,15 @@ public class OrderProducerTest {
 
         String[] statusName = {"已创建", "已付款", "已配送", "已取消", "已完成"};
 
-        // 模拟订单
+        // 模拟订单消息
         for (int orderId = 0; orderId < 10; orderId++) {
+            // 模拟订单的每个状态来发送消息
             for (int j = 0; j < statusName.length; j++) {
                 String messageContent = new OrderMessage().setId(orderId).setStatus(statusName[j])
                         .setSendOrder(j).setContent("hello orderly rocketMQ message!").toString();
-                Message sendMessage = new Message(topicName, statusName[j], orderId + "#" + statusName[j],
+                Message sendMessage = new Message(topicName, // 消息主题
+                        statusName[j], // 每个状态一个标签
+                        orderId + "#" + statusName[j], // 自定义消息的key，常用于消息去重处理
                         messageContent.getBytes(RemotingHelper.DEFAULT_CHARSET));
 
                 // 发送消息并返回消息
